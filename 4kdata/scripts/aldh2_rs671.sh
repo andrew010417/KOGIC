7a7c4ac64d7f099b09885b49e6516011ae1d5cf4
andrew010417 <p010417pjh@gmail.com>
2026-02-02 17:36:50 +0900

    Update aldh2_rs671.sh

--- 4kdata/scripts/aldh2_rs671.sh
+++ 4kdata/scripts/aldh2_rs671.sh
@@ -8,7 +8,7 @@ BCFTOOLS="/BiO/Access/jaehyung/tools/bcftools-1.18/bcftools"
 # ↑ PATH 안 타도 항상 실행 가능
 
 # chr12 VCF 파일
-VCF="/BiO/Access/jaehyung/myfirstjob/4krawdata/Korea4K.Final/chr12.recal.vcf"
+VCF="/BiO/Access/jaehyung/myfirstjob/4kdata/Korea4K.Final/chr12.recal.vcf"
 
 # 출력 파일
 OUT="results/rs671.vcf"
